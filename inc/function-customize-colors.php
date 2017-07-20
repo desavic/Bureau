16d690db3bf06e2c1e0941be0d91a367694f0871
desavic <desavic@gmail.com>
2017-07-20 21:46:20 +0200

    Develop changes

--- inc/function-customize-colors.php
+++ inc/function-customize-colors.php
@@ -3,17 +3,18 @@
 @package bureau theme
 
     =========================
-        ARANGE COLORS PAGE
+        ARANGE PAGE COLORS
     =========================
 
 	Registering control settings and adding controls to header background,
-	footer background and main menu.
+	footer background and main menu and customizing CSS
 */
 function bureau_customize_register( $wp_customize ){
 
 /*
-	Add settings to header background, footer background and main menu color controls
+	Add settings to header background, footer background and main menu text color controls
 */
+	//Add settings to header background
 	$wp_customize->add_setting('bureau-header_background_color',
 		array(
 			'default'           =>  '#636363',
@@ -21,6 +22,7 @@ function bureau_customize_register( $wp_customize ){
 			'transport'         => 'postMessage',
 		)
 	);
+	//Add settings to  footer background
 	$wp_customize->add_setting('bureau-footer_background_color',
 		array(
 			'default'           =>  '#636363',
@@ -28,6 +30,7 @@ function bureau_customize_register( $wp_customize ){
 			'transport'         => 'postMessage',
 		)
 	);
+	//Add settings to main menu text color controls
     $wp_customize->add_setting('bureau-header_nav_text_color',
 		array(
 			'default'           =>  '#e5e5e5',
@@ -37,8 +40,9 @@ function bureau_customize_register( $wp_customize ){
 	);
 
 /*
-	Add controls to header background, footer background and main menu color controls
+	Add controls to header background, footer background and main menu text color controls
 */
+	//Add controls for header background color
 	$wp_customize->add_control( new WP_Customize_Color_Control(
 		$wp_customize,'bureau-header_background_color_control',
 			array(
@@ -49,6 +53,7 @@ function bureau_customize_register( $wp_customize ){
 			)
 		)
 	);
+	//Add controls for footer background color
 	$wp_customize->add_control( new WP_Customize_Color_Control(
 		$wp_customize,'bureau-footer_background_color_control',
 			array(
@@ -59,6 +64,7 @@ function bureau_customize_register( $wp_customize ){
 			)
 		)
 	);
+	//Add controls for main menu text color controls
     $wp_customize->add_control( new WP_Customize_Color_Control(
 		$wp_customize,'bureau-header_nav_text_color_control',
 			array(
@@ -73,6 +79,9 @@ function bureau_customize_register( $wp_customize ){
 }
 add_action('customize_register', 'bureau_customize_register');
 
+/*
+	Customizing CSS output for header background, footer background and main menu text color
+*/
 function bureau_customize_css_output(){   ?>
 	<style type="text/css">
 
